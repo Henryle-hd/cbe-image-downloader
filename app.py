09044406679d97bd8532ec8b32ba4a28a66bf7d1
Henryle-hd <henrydionizi@gmail.com>
2025-06-06 12:30:22 +0300

    meta-data

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ import streamlit as st
 from cbe_image import fetch_image
 import requests
 
+st.set_page_config(
+    page_title="CBE Image Downloader",
+    page_icon="📸",
+    layout="centered",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://henrylee-hd.vercel.app/',
+        'Report a bug': "https://github.com/Henryle-hd/cbe-image-downloader/issues",
+        'About': "# CBE Image Downloader\nThis app helps you download student images from CBE database."
+    }
+)
+
 st.title("CBE Image Downloader")
 
 student_id = st.text_input("Enter Student ID:")
